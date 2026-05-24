b69bd8da2766ad3b39aa04168de435ff79b05598
Mario <marioalbaladejomartinez@gmail.com>
2026-05-24 11:39:24 +0200

    Bucle
    Creaciñon de bucle para que la app no se salga y permanezca buscando
    entrada.

--- src/calc.java
+++ src/calc.java
@@ -3,5 +3,16 @@ package src;
 public class calc {
   public static void main(String[] args) {
 
+    String input = "null";
+
+    System.out.println(
+        "Bienvenido a mi calculadora \nPara usar la calculadora debes de introducir los numeros en una sola linea: \nEjemplo: 2+2*4");
+    while (!input.equals("quit")) {
+      input = IO.readln();
+      if (input.equals("quit")) {
+        break;
+      }
+
+    }
   }
 }
