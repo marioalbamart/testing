6020ada3ab405b03657893d35e46fee4b403d5e9
Mario <marioalbaladejomartinez@gmail.com>
2026-05-26 20:32:59 +0200

    feat: bucles
    añadido de bucles que manejan stringbuilders para luego convertirlos en
    int y case para saber cual es el operador.

--- src/calc.java
+++ src/calc.java
@@ -3,16 +3,82 @@ package src;
 public class calc {
   public static void main(String[] args) {
 
-    String input = "null";
+    /*
+     * String a = "123";
+     * int b = Integer.parseInt(a);
+     * System.out.println(b);
+     */
 
+    boolean bucle = false;
+    StringBuilder num1 = new StringBuilder();
+    StringBuilder num2 = new StringBuilder();
+    char caseOpt;
+    String opt = "null";
+
+    // intro de app
     System.out.println(
-        "Bienvenido a mi calculadora \nPara usar la calculadora debes de introducir los numeros en una sola linea: \nEjemplo: 2+2*4");
-    while (!input.equals("quit")) {
-      input = IO.readln();
-      if (input.equals("quit")) {
-        break;
+        "Bienvenido a mi calculadora.\nIntroduzca dos numeros y un operador.\nEjemplo: 2+2\nPara salir pulse 'q' + enter\n===========================");
+
+    while (bucle != true) { // bucle encargado de mantener la app ejecutandose
+      caseOpt = 'a';
+      num1.delete(0, num1.length());
+      num2.delete(0, num2.length());
+
+      String input = IO.readln(); // Entrada de la operación ej(2+2)
+
+      // bucle encargado de gestionar los inputs dependiendo de cual sea el tipo de
+      // entrada.
+      // digit alphabet ...
+      for (var i = 0; i < input.length(); i++) {
+
+        if (input.charAt(i) == 'q') {
+          bucle = true;
+          break;
+        }
+
+        switch (input.charAt(i)) {
+          case '+':
+            opt = "sum";
+            caseOpt = 'b';
+            break;
+
+          case '-':
+            opt = "res";
+            caseOpt = 'b';
+            break;
+
+          case '/':
+            opt = "div";
+            caseOpt = 'b';
+            break;
+
+          case '*':
+            opt = "mul";
+            caseOpt = 'b';
+            break;
+
+          default:
+            break;
+        }
+
+        switch (caseOpt) {
+          case 'a':
+            if (Character.isDigit(input.charAt(i))) {
+              num1.append(input.charAt(i));
+            }
+            break;
+
+          case 'b':
+            if (Character.isDigit(input.charAt(i))) {
+              num2.append(input.charAt(i));
+            }
+
+          default:
+            break;
+        }
       }
 
+      System.out.println(num1 + " " + opt + " " + num2);
     }
   }
 }
